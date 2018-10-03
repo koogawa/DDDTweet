d6adc566d07d3e1e9675b8f06e61cae5783fc0ad
Kosuke Ogawa <ogawa_kousuke@aratana.jp>
2018-10-03 16:03:42 +0900

    Initial Commit

---
+++ Twitter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Twitter
+//
+//  Created by ogawa_kousuke on 2018/10/03.
+//  Copyright © 2018年 Aratana inc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
